501464aed7094cd2f07d6f142d38c40bdf340670
정창휘 <jch9937@gmail.com>
2024-05-02 05:45:05 +0900

    feat: 각 controller 추가/ space 조회,생성 기능
    Change-Id: I9a04dfb7fd88d0a09827694a247c4eeabe4194d8

---
+++ note/src/main/java/com/sixb/note/api/controller/NoteController.java
@@ -0,0 +1,9 @@
+package com.sixb.note.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/note")
+public class NoteController {
+}
