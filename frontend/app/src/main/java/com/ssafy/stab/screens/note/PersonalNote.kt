25311fbc3c765b9f7671849847d7c7b01dcc7ce4
김해인 <khi762@naver.com>
2024-05-02 03:23:05 +0900

    feat: 노트 에디터 ui 추가
    Change-Id: Iac85540def16fae6f80817fce6f79dc718920ff5

--- frontend/app/src/main/java/com/ssafy/stab/screens/note/PersonalNote.kt
+++ frontend/app/src/main/java/com/ssafy/stab/screens/note/PersonalNote.kt
@@ -6,26 +6,42 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.ssafy.stab.components.note.ControlsBar
 import com.ssafy.stab.util.note.NoteArea
 import com.ssafy.stab.util.note.rememberNoteController
 
 @Composable
 fun PersonalNote(navController: NavController){
     val noteController = rememberNoteController()
+    val undoAvailable = remember { mutableStateOf(false) }
+    val redoAvailable = remember { mutableStateOf(false) }
+    val penType = remember { mutableStateOf("pen") }
 
-    Box {
-        Column {
-            Row {
-                Button(onClick = { navController.popBackStack() }) {
-                    Text(text = "뒤로가기")
-                }
+    Column {
+        Row {
+            Button(onClick = { navController.popBackStack() }) {
+                Text(text = "뒤로가기")
             }
-            NoteArea(
-                noteController = noteController,
-            )
+        }
+
+        ControlsBar(
+            noteController = noteController,
+            undoAvailable = undoAvailable,
+            redoAvailable = redoAvailable,
+            penType = penType
+        )
+
+        NoteArea(
+            noteController = noteController,
+        ) {
+            undoCount, redoCount ->
+            undoAvailable.value = undoCount != 0
+            redoAvailable.value = undoCount != 0
         }
     }
 
