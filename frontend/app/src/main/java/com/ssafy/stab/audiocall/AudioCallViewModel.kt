e56d429c1f37b18ec9f2a06be475bc3e99d00cda
탁윤희 <gwalgwal98@naver.com>
2024-05-02 14:52:09 +0900

    feat: mediasoup-client 사용자 device 미디어 설정(현재 라이브러리에 필요한 so 파일이 없어 주석 처리)
    Change-Id: I5f98c1b91ab4f0d9e05646501ebacae7d79df929

--- frontend/app/src/main/java/com/ssafy/stab/audiocall/AudioCallViewModel.kt
+++ frontend/app/src/main/java/com/ssafy/stab/audiocall/AudioCallViewModel.kt
@@ -6,10 +6,13 @@ import io.socket.client.Ack
 import io.socket.client.IO
 import io.socket.client.Socket
 import org.json.JSONObject
+import org.mediasoup.droid.Device
 import java.net.URISyntaxException
 
+// 연결 상태 확인 로그 추후 삭제 요망
 class AudioCallViewModel : ViewModel() {
     private var socket: Socket? = null // 클라이언트 소켓 인스턴스 생성 - socket 통신
+//    private var device: Device? = null // 클라이언트 단말 기기 미디어 설정을 위한 디바이스 - webRTC 통신
 
     // 소켓 서버 초기화
     init {
@@ -47,6 +50,7 @@ class AudioCallViewModel : ViewModel() {
                 // { rtpCapabilities } 형태로 오므로 JSONObject로 처리한다.
                 val rtpCapabilities = args[0] as JSONObject
                 // 서버에서 받은 rtpCapabilities를 이용하여 Device 설정
+//                initializeDevice(rtpCapabilities)
 
                 Log.d("AudioCallViewModel", "Successfully joined room: $roomName with RTP Capabilities: $rtpCapabilities")
 
@@ -56,11 +60,25 @@ class AudioCallViewModel : ViewModel() {
         })
     }
 
+//    private fun initializeDevice(rtpCapabilities: JSONObject) {
+//        device = Device()
+//        try {
+//            // p2p 연결이 아닌 sfu 구조를 사용해 peerConnectionOptions 가 필요 없다, webRTC 기본 설정 사용(==null)
+//            device?.load(rtpCapabilities.toString(), null)
+////            createSendTransport()
+//        } catch (e: Exception) {
+//            Log.e("AudioCallViewModel", "Error loading device: ${e.message}")
+//        }
+//    }
+
+//    private fun createSendTransport() {
+//        // transport 생성
+//    }
+
     // 소켓 서버와의 연결 끊기
     override fun onCleared() {
         super.onCleared()
         socket?.disconnect()
         Log.d("AudioCallViewModel", "Socket disconnected.")
     }
-
 }
