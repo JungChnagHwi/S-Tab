e6da4dec5a16fea6659719cc092e78f35de5127c
김정민 <kjm1994@naver.com>
2024-05-02 22:18:17 +0900

    remove: Note 서버 Auth 코드 제거
    Change-Id: I7591b5cb65093b2fff359784f3d4351d55982914

--- note/src/main/java/com/sixb/note/repository/BlackListRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sixb.note.repository;
-
-import com.sixb.note.entity.BlackList;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BlackListRepository extends CrudRepository<BlackList, String> {
-}
--- note/src/main/java/com/sixb/note/repository/RefreshTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sixb.note.repository;
-
-import com.sixb.note.entity.RefreshToken;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-
-    RefreshToken findByUserId(long userId);
-}
