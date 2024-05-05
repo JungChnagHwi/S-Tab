1113129779170bc9c3d5a2d32334e6309100d889
김정민 <kjm1994@naver.com>
2024-05-05 23:01:20 +0900

    feat: GPT 서버
    python의 langchain 사용, fastapi 서버 구축
    유저별 대화 내용을 memory에 저장하여 관리
    100개 이상의 대화 내용이 쌓이면 대화 내용을 요약해서 관리
    
    Change-Id: I6f886052286d01669642c9c60c383223646d88e7

---
+++ gpt/gpt.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI
+from langchain_openai import ChatOpenAI
+from langchain.memory import ConversationSummaryBufferMemory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.schema.runnable import RunnablePassthrough
+from py_eureka_client import eureka_client
+from dotenv import load_dotenv
+import os
+
+app = FastAPI()
+
+@app.on_event("startup")
+def startup_event():
+    load_dotenv()
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+    eureka_server_url = os.getenv("EUREKA_SERVER_URL")
+    app_name = "gpt"
+    instance_port = 8003
+
+    global llm
+    llm = ChatOpenAI(
+        openai_api_key=openai_api_key,
+        temperature=0.1
+    )
+
+    eureka_client.init(eureka_server=eureka_server_url,
+                       app_name=app_name,
+                       instance_id=app_name,
+                       instance_port=instance_port
+    )
+
+@app.get("/api/gpt")
+async def chat(q: str, user_id: int):
+    memory_key = f"chat_history_{user_id}"
+
+    memory = ConversationSummaryBufferMemory(
+        llm=llm,
+        max_token_limit=100,
+        memory_key=memory_key,
+        return_messages=True
+    )
+
+    chat_history = memory.load_memory_variables({})[memory_key]
+
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "당신은 학생들의 질문을 아주 친절하고 자세하게 답변할 수 있는 AI 챗봇이다."),
+        MessagesPlaceholder(variable_name=memory_key),
+        ("user", "{question}")
+    ])
+
+    chain = RunnablePassthrough.assign(chat_history=chat_history) | prompt | llm
+
+    result = chain.invoke({"question": q})
+
+    memory.save_context(
+        {"input": q},
+        {"output": result.content}
+    )
+
+    return {"question": q, "answer": result.content}
+
+
