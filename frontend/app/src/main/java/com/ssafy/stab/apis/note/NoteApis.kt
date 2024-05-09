cbf1982c4929d60537a353d5c4bb8b018882db71
김해인 <khi762@naver.com>
2024-05-09 09:49:42 +0900

    feat: 페이지 조회 api 설정
    Change-Id: I02b683dfaf314064084dec40615f427a36752dcc

--- frontend/app/src/main/java/com/ssafy/stab/apis/note/NoteApis.kt
+++ frontend/app/src/main/java/com/ssafy/stab/apis/note/NoteApis.kt
@@ -10,12 +10,37 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+private val apiService: ApiService = RetrofitClient.instance.create(ApiService::class.java)
+private val accessToken = PreferencesUtil.getLoginDetails().accessToken
+private val authorizationHeader = "Bearer $accessToken"
+fun fetchPageList(
+    noteId: String = "n-2cb49f1d-9d2a-4d1d-a8e2-9272f304c734",
+    onResponseSuccess: (PageListResponse) -> Unit
+) {
+    val call = apiService.getPageList(authorizationHeader, noteId)
 
+    call.enqueue(object : Callback<PageListResponse> {
+        override fun onResponse(
+            call: Call<PageListResponse>,
+            response: Response<PageListResponse>
+        ) {
+            if (response.isSuccessful) {
+                val responseBody = response.body()!!
+                Log.d("response", responseBody.toString())
+                onResponseSuccess(responseBody)
+                Log.i("fetchPageList", "isSuccessful")
+            } else {
+                Log.e("fetchPageList", "${response.code()}: ${response.errorBody()?.string()}")
+            }
+        }
+
+        override fun onFailure(call: Call<PageListResponse>, t: Throwable) {
+            Log.e("fetchPageList", "err: ${t.localizedMessage}")
+        }
+    })
+}
 
 fun createNewNote(noteRequest: NoteRequest) {
-    val apiService = RetrofitClient.instance.create(ApiService::class.java)
-    val accessToken = PreferencesUtil.getLoginDetails().accessToken
-    val authorizationHeader = "Bearer $accessToken"
     val call = apiService.createNote(authorizationHeader, noteRequest)
 
     call.enqueue(object : Callback<NoteResponse> {
--- frontend/app/src/main/java/com/ssafy/stab/screens/note/PersonalNote.kt
+++ frontend/app/src/main/java/com/ssafy/stab/screens/note/PersonalNote.kt
@@ -1,61 +1,43 @@
 package com.ssafy.stab.screens.note
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.ssafy.stab.apis.note.fetchPageList
 import com.ssafy.stab.components.note.ControlsBar
 import com.ssafy.stab.components.note.OptionsBar
 import com.ssafy.stab.components.note.PageList
-import com.ssafy.stab.components.note.Template
 import com.ssafy.stab.data.note.BackgroundColor
 import com.ssafy.stab.data.note.Coordinate
-import com.ssafy.stab.data.note.Direction
 import com.ssafy.stab.data.note.PathInfo
 import com.ssafy.stab.data.note.PenType
-import com.ssafy.stab.data.note.response.Page
-import com.ssafy.stab.data.note.response.Response
 import com.ssafy.stab.data.note.TemplateType
 import com.ssafy.stab.data.note.response.PageData
 import com.ssafy.stab.data.note.response.PageDetail
 import com.ssafy.stab.data.note.response.PageListResponse
 import com.ssafy.stab.ui.theme.Background
-import com.ssafy.stab.ui.theme.NoteAreaBackground
-import com.ssafy.stab.util.note.NoteArea
-import com.ssafy.stab.util.note.getTemplate
 import com.ssafy.stab.util.note.rememberNoteController
-import kotlinx.coroutines.launch
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 @SuppressLint("UnrememberedMutableState")
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun PersonalNote(navController: NavController){
     val noteController = rememberNoteController()
@@ -86,7 +68,13 @@ fun PersonalNote(navController: NavController){
             mutableListOf(), mutableListOf(), mutableListOf()
         )),
     )
-    val response = PageListResponse(data = data)
+
+    val response = mutableStateOf<PageListResponse?>(null)
+
+    response.value = PageListResponse(data = data)
+//    fetchPageList() {
+//        response.value = it
+//    }
 
     Column(
         modifier = Modifier
@@ -114,7 +102,9 @@ fun PersonalNote(navController: NavController){
             OptionsBar(noteController = noteController)
         }
 
-        PageList(response, noteController)
+        if (response.value != null) {
+            PageList(response.value!!, noteController)
+        }
 
     }
 }
