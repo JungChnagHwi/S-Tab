b0fee47d8e9b2d0bb8db35464ab861071798b664
김연빈 <kyb1434@gmail.com>
2024-05-13 10:36:53 +0900

    remove: 안쓰는 import문 삭제
    Change-Id: I1b3c3efacaee859b9f7593d84fa1376797f6ce3e

--- note/src/main/java/com/sixb/note/dto/pageData/PageDataDto.java
+++ note/src/main/java/com/sixb/note/dto/pageData/PageDataDto.java
@@ -2,7 +2,6 @@ package com.sixb.note.dto.pageData;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
