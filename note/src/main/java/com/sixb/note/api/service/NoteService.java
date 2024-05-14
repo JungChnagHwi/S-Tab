59458101af60943d872862b1911e4baf2bde1ecd
김정민 <kjm1994@naver.com>
2024-05-14 17:55:20 +0900

    fix: note 생성 시 cypher dsl 사용
    Change-Id: I875eae221dc436f1fe25e58b76d02a17cb4d6eec

--- note/src/main/java/com/sixb/note/api/service/NoteService.java
+++ note/src/main/java/com/sixb/note/api/service/NoteService.java
@@ -3,91 +3,25 @@ package com.sixb.note.api.service;
 import com.sixb.note.dto.note.CreateNoteRequestDto;
 import com.sixb.note.dto.note.CreateNoteResponseDto;
 import com.sixb.note.dto.note.RelocateNoteRequestDto;
-import com.sixb.note.entity.Folder;
 import com.sixb.note.entity.Note;
-import com.sixb.note.entity.Page;
 import com.sixb.note.exception.FolderNotFoundException;
 import com.sixb.note.exception.NotFoundException;
 import com.sixb.note.exception.NoteNotFoundException;
-import com.sixb.note.repository.FolderRepository;
 import com.sixb.note.repository.NoteRepository;
-import com.sixb.note.util.Const;
-import com.sixb.note.util.IdCreator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class NoteService {
 
 	private final NoteRepository noteRepository;
-	private final FolderRepository folderRepository;
 
 	@Transactional
 	public CreateNoteResponseDto createNote(CreateNoteRequestDto request) throws FolderNotFoundException {
-		String parentFolderId = request.getParentFolderId();
-		Folder parentFolder = folderRepository.findFolderById(parentFolderId);
-
-		if (parentFolder == null) {
-			throw new FolderNotFoundException("존재하지 않는 폴더입니다.");
-		}
-
-		LocalDateTime now = LocalDateTime.now();
-		String formattedNoteId = IdCreator.create("n");
-
-		// 새로운 페이지 생성
-		Page page = Page.builder()
-				.pageId(IdCreator.create("p"))
-				.noteId(formattedNoteId)
-				.template(request.getTemplate())
-				.color(request.getColor())
-				.direction(request.getDirection())
-				.createdAt(now)
-				.updatedAt(now)
-				.pageData(Const.INIT_PAGE_DATA)
-				.build();
-
-		// 새로운 노트 생성
-		Note note = Note.builder()
-				.title(request.getTitle())
-				.totalPageCnt(1)
-				.noteId(formattedNoteId)
-				.createdAt(now)
-				.updatedAt(now)
-				// 노트와 페이지 연결
-				.page(page)
-				.build();
-
-		// 노트를 부모 폴더에 연결
-		parentFolder.getNotes().add(note);
-		folderRepository.save(parentFolder);
-
-		// 저장
-		noteRepository.save(note);
-
-		// 응답 DTO 구성
-		return CreateNoteResponseDto.builder()
-				.noteId(note.getNoteId())
-				.title(note.getTitle())
-				.totalPageCnt(note.getTotalPageCnt())
-				.isLiked(false)
-				.createdAt(note.getCreatedAt())
-				.createdAt(note.getUpdatedAt())
-				.isDeleted(false)
-				.page(CreateNoteResponseDto.PageDto.builder()
-						.pageId(page.getPageId())
-						.color(page.getColor())
-						.template(page.getTemplate())
-						.direction(page.getDirection())
-						.isBookmarked(false)
-						.createdAt(page.getCreatedAt())
-						.updatedAt(page.getUpdatedAt())
-						.isDeleted(false)
-						.build())
-				.build();
+		return noteRepository.createNote(request)
+				.orElseThrow(() -> new IllegalArgumentException("노트 생성에 실패했습니다."));
 	}
 
 	//노트 이름 변경
---
+++ note/src/main/java/com/sixb/note/repository/NoteRepositoryCustomImpl.java
@@ -0,0 +1,126 @@
+package com.sixb.note.repository;
+
+import com.sixb.note.dto.note.CreateNoteRequestDto;
+import com.sixb.note.dto.note.CreateNoteResponseDto;
+import com.sixb.note.exception.FolderNotFoundException;
+import com.sixb.note.util.Const;
+import com.sixb.note.util.IdCreator;
+import lombok.RequiredArgsConstructor;
+import org.neo4j.cypherdsl.core.Node;
+import org.neo4j.cypherdsl.core.Relationship;
+import org.neo4j.cypherdsl.core.Statement;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.Record;
+import org.neo4j.driver.Result;
+import org.neo4j.driver.Session;
+import org.neo4j.driver.types.MapAccessor;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+import static org.neo4j.cypherdsl.core.Cypher.*;
+
+@Repository
+@RequiredArgsConstructor
+public class NoteRepositoryCustomImpl implements NoteRepositoryCustom {
+
+	private final Driver driver;
+
+	@Override
+	public Optional<CreateNoteResponseDto> createNote(CreateNoteRequestDto request) throws FolderNotFoundException {
+		LocalDateTime now = LocalDateTime.now();
+
+		String spaceId = getSpaceId(request.getParentFolderId())
+				.orElseThrow(() -> new FolderNotFoundException("존재하지 않는 폴더입니다."));
+		String noteId = IdCreator.create("n");
+		String pageId = IdCreator.create("p");
+
+		Node folder = node("Folder").named("f")
+				.withProperties("folderId", literalOf(request.getParentFolderId()));
+
+		Node note = node("Note").named("n")
+				.withProperties(
+						"noteId", literalOf(noteId),
+						"spaceId", literalOf(spaceId),
+						"title", literalOf(request.getTitle()),
+						"totalPageCnt", literalOf(1),
+						"createdAt", literalOf(now),
+						"updatedAt", literalOf(now),
+						"isDeleted", literalFalse());
+
+		Node page = node("Page").named("p")
+				.withProperties(
+						"pageId", literalOf(pageId),
+						"noteId", literalOf(noteId),
+						"template", literalOf(request.getTitle()),
+						"color", literalOf(request.getColor()),
+						"direction", literalOf(request.getDirection()),
+						"pdfUrl", literalOf(null),
+						"pdfPage", literalOf(0),
+						"pageData", literalOf(Const.INIT_PAGE_DATA),
+						"createdAt", literalOf(now),
+						"updatedAt", literalOf(now),
+						"isDeleted", literalFalse());
+
+		Relationship hierarchy = folder.relationshipTo(note, "Hierarchy");
+		Relationship nextPage = note.relationshipTo(page, "NextPage");
+
+		Statement statement = match(folder)
+				.create(note, page, hierarchy, nextPage)
+				.returning(note, page)
+				.build();
+
+		CreateNoteResponseDto response = null;
+
+		try (Session session = driver.session()) {
+			Result result = session.run(statement.getCypher());
+			if (result.hasNext()) {
+				Record record = result.next();
+
+				MapAccessor noteNode = record.get("n").asNode();
+				MapAccessor pageNode = record.get("p").asNode();
+
+				CreateNoteResponseDto.PageDto pageDto = CreateNoteResponseDto.PageDto.builder()
+						.pageId(pageNode.get("pageId").asString())
+						.color(pageNode.get("color").asString())
+						.template(pageNode.get("template").asString())
+						.direction(pageNode.get("direction").asInt())
+						.isBookmarked(false)
+						.createdAt(pageNode.get("createdAt").asLocalDateTime())
+						.updatedAt(pageNode.get("updatedAt").asLocalDateTime())
+						.isDeleted(pageNode.get("isDeleted").asBoolean())
+						.build();
+
+				response = CreateNoteResponseDto.builder()
+						.noteId(noteNode.get("noteId").asString())
+						.title(noteNode.get("title").asString())
+						.totalPageCnt(noteNode.get("totalPageCnt").asInt())
+						.isLiked(false)
+						.page(pageDto)
+						.createdAt(noteNode.get("createdAt").asLocalDateTime())
+						.updatedAt(noteNode.get("updatedAt").asLocalDateTime())
+						.isDeleted(noteNode.get("isDeleted").asBoolean())
+						.build();
+			}
+		}
+
+		return Optional.ofNullable(response);
+	}
+
+	private Optional<String> getSpaceId(String folderId) {
+		Node folder = node("Folder").named("f")
+				.withProperties("folderId", literalOf(folderId));
+
+		Statement statement = match(folder)
+				.returning(folder.property("spaceId").as("spaceId"))
+				.build();
+
+		try (Session session = driver.session()) {
+			Result result = session.run(statement.getCypher());
+			String ret = result.single().get("spaceId").asString();
+			return Optional.ofNullable(ret);
+		}
+	}
+
+}
