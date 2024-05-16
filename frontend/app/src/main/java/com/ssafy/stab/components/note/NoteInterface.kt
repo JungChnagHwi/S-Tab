2f1833ce1cf4c1ef46cb91a19969b8bc985ff991
김연빈 <kyb1434@gmail.com>
2024-05-16 21:00:58 +0900

    feat: 내보내기 아이콘 추가
    Change-Id: I67c0cf2561e8dc1d32ae1ac87b112ea484c7b645

--- frontend/app/src/main/java/com/ssafy/stab/components/note/NoteInterface.kt
+++ frontend/app/src/main/java/com/ssafy/stab/components/note/NoteInterface.kt
@@ -1,14 +1,24 @@
 package com.ssafy.stab.components.note
 
 import androidx.annotation.DrawableRes
+import androidx.compose.animation.expandVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.ssafy.stab.R
@@ -39,6 +49,27 @@ fun PageInterfaceBar(
             R.drawable.ellipsis_horizontal,
             "setting"
         ) {
+
+        }
+        var expanded by remember { mutableStateOf(false) }
+        OptionIcons(
+            R.drawable.export,
+            "export"
+        ) {
+            expanded = true
+        }
+        DropdownMenu(expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                text = {
+                    Text(text="이미지로 저장하기 (.png)")
+                },
+                onClick = { 
+                    expanded = false
+                    // 이미지 저장하기
+                    // saveAsImage()
+                })
 
         }
     }
