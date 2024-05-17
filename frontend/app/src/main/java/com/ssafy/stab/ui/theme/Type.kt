7d8d9bf1c3d8a7060a7b1fb4730e031e6719a41f
탁윤희 <gwalgwal98@naver.com>
2024-05-18 06:59:02 +0900

    style: ui font thema 설정
    Change-Id: Id94a576f0bf5a2be9749b1711419b7fb0c108566

--- frontend/app/src/main/java/com/ssafy/stab/ui/theme/Type.kt
+++ frontend/app/src/main/java/com/ssafy/stab/ui/theme/Type.kt
@@ -2,14 +2,23 @@ package com.ssafy.stab.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.ssafy.stab.R
+
+val pretendardFamily = FontFamily(
+    Font(R.font.pretendard_medium, FontWeight.Medium),
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold),
+    Font(R.font.pretendard_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = pretendardFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
