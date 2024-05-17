82131bd99469fd7fdd1929706f83a7c707c8d1e5
탁윤희 <gwalgwal98@naver.com>
2024-05-18 07:26:10 +0900

    style: 폴더, 노트 생성 옵션 사이에 선 추가
    Change-Id: I6302840c0e8f7ea3b301c533faca78a86fa8e116

--- frontend/app/src/main/java/com/ssafy/stab/screens/space/NoteListSpace.kt
+++ frontend/app/src/main/java/com/ssafy/stab/screens/space/NoteListSpace.kt
@@ -1,22 +1,21 @@
 package com.ssafy.stab.screens.space
 
 import NoteListViewModelFactory
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -192,7 +191,11 @@ fun ListGridScreen(
                             showCreateOptions.value = false
                             showFolderModal.value = true
                         })
-                        Spacer(modifier = Modifier.height(15.dp))
+                        Divider(
+                            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f),
+                            thickness = 1.dp,
+                            modifier = Modifier.padding(vertical = 8.dp)
+                        )
                         Text("노트 생성", modifier = Modifier.clickable {
                             showCreateOptions.value = false
                             showNoteModal.value = true
